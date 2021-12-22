8e0ebfa9fbc7413c286aec3245b50e07e43a7fc2
Artur Socha <asocha@redhat.com>
2021-12-22 16:51:48 +0100

    Setup keycloak to run on ovirt engine's wildfly instance (#2)
    * Use keycloak overlay to build RPMs
    
    Bug-Url: https://bugzilla.redhat.com/2021497
    Signed-off-by: Artur Socha <asocha@redhat.com>
    Change-Id: Ie07423fc6f4788e390dc24e1d3a78d0fa0292294
    
    * Keycloak running on ovirt engine wildfly
    
    The goal of this patch is to setup Keycloak overlay to run on Ovirt
    Engine's Wildfly/EAP instance.
    The activation takes place automatically when engine-setup is run and
    ovirt-engine-keyclaok-setup & ovirt-engine-keycloak packages are
    installed.
    As a prerequisite ovirt-engine's patch [1] providing Wildfly/EAP
    configuration template must be installed.
    
    [1] https://gerrit.ovirt.org/c/ovirt-engine/+/117722
    
    Bug-Url: https://bugzilla.redhat.com/2021497
    Signed-off-by: Artur Socha <asocha@redhat.com>

--- .automation/build-srpm.sh
+++ .automation/build-srpm.sh
@@ -2,7 +2,20 @@
 
 # Directory, where build artifacts will be stored, should be passed as the 1st parameter
 ARTIFACTS_DIR=${1:-exported-artifacts}
+export ARTIFACTS_DIR
 
 # Prepare source archive
 [[ -d rpmbuild/SOURCES ]] || mkdir -p rpmbuild/SOURCES
 
+# Clean leftovers from previous builds
+rm -rf rpmbuild/SOURCES/*
+make clean
+
+# Get the tarball
+make dist
+
+
+# Build SRPMs
+rpmbuild \
+    -D "_topdir rpmbuild" \
+    -ts ./*.tar.gz
